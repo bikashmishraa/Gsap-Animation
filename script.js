4066884d67766c58a8a2b48cd0fb11f7fedb5420
Bikash C. Mishra <99139380+bikashmishraa@users.noreply.github.com>
2024-01-12 01:22:36 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 var tl=gsap.timeline();
 
+var a=0;
+function time(){
+    setInterval(function(){
+    a+= Math.floor(Math.random()*15);
+    if(a<100){
+        document.querySelector(".loader h1").innerHTML = a + "%";
+    }
+    else{
+        a=100;
+        document.querySelector(".loader h1").innerHTML = a + "%";
+    }
+},100)
+
+}
+tl.to(".loader h1",{
+    scale:3,
+    duration:1,
+    delay:0.1,
+})
+tl.to(".loader",{
+    top:"-100%",
+    duration:2,
+    delay:1,
+    onStart:time(),
+})
     tl.from(".navbar img",{
         // x:-1000,
         duration:4,
